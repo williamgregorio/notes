25253686a74213fa144512484c473c69f38caa82
William Gregorio <mrwilliamgregorio@yahoo.com>
2023-07-02 19:26:49 -0400

    remix init starting

---
+++ app/app/routes/_index.jsx
@@ -0,0 +1,14 @@
+export const meta = () => {
+  return [
+    { title: "Notes" },
+    { name: "description", content: "Quick notes app" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
