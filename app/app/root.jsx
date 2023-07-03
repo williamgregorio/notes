65eb9b3fb9582b3e15712b7c59924940dcc4e71e
William Gregorio <mrwilliamgregorio@yahoo.com>
2023-07-02 22:51:25 -0400

    remix init root main

--- app/app/root.jsx
+++ app/app/root.jsx
@@ -1,4 +1,10 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
+import styles from '~/styles/style.css';
+import MainNavigation from './components/MainNavigation';
+import { Link } from '@remix-run/react';
+import {
+  useRouteError,
+  isRouteErrorResponse,
+} from "@remix-run/react";
 
 import {
   Links,
@@ -10,7 +16,7 @@ import {
 } from "@remix-run/react";
 
 export const links = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  ...([{ rel: "stylesheet", href: styles }]),
 ];
 
 export default function App() {
@@ -23,11 +29,64 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <MainNavigation />
         <Outlet />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
+  );
+}
+
+//errors : remix only v2
+
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  // when true, this is what used to go to `CatchBoundary`
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div>
+        <h1>Oops</h1>
+        <p>Status: {error.status}</p>
+        <p>{error.data.message}</p>
+      </div>
+    );
+  }
+
+  // Don't forget to typecheck with your own logic.
+  // Any value can be thrown, not just errors!
+  let errorMessage = "Unknown error";
+  const isDefinitelyAnError = () => {
+    return true;
+  }
+  if (isDefinitelyAnError(error)) {
+    errorMessage = error.message;
+  }
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <MainNavigation />
+        <section className='error'>
+          <h1>Uh ahh ...</h1>
+          <p>Something went wrong.</p>
+          <p>{errorMessage}</p>
+          <p>Come back to <Link to="/">safety</Link></p>
+        </section>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+    
   );
 }
