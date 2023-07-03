966ce2aa29936261786b8dad56efccc8bf2e4c21
William Gregorio <mrwilliamgregorio@yahoo.com>
2023-07-02 22:53:14 -0400

    index append init

--- app/app/routes/_index.jsx
+++ app/app/routes/_index.jsx
@@ -1,3 +1,4 @@
+import {Link} from '@remix-run/react';
 export const meta = () => {
   return [
     { title: "Notes" },
@@ -7,8 +8,18 @@ export const meta = () => {
 
 export default function Index() {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <section id="hero">
+      <div className='hero-action'>
+        <h1>Track your notes <span id="headingOneCap">ON THE GO</span></h1>
+      <p>Try out what you've been missing out on. Watch your production sky rocket today</p>
+      <div>
+        <Link to="/notes" className='go-btn'>Go</Link>
+      </div>
+      </div>
+      <div className='hero-image'>
+        <img src='https://images.pexels.com/photos/3769021/pexels-photo-3769021.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' />
+      </div>
+      
+    </section>
   );
 }
